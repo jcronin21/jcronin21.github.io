07f91e16a6c4857dc51f20ef0e721f30004af5bc
Jazz Cronin <123016934+jcronin21@users.noreply.github.com>
2023-05-01 11:35:21 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,88 @@
+import {getAccessToken} from './utilities.js';
+
+const rootURL = 'https://photo-app-secured.herokuapp.com';
+
+const showStories = async (token) => {
+    const endpoint = `${rootURL}/api/stories`;
+    const response = await fetch(endpoint, {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token
+        }
+    })
+    const data = await response.json();
+    console.log(data);
+    const htmlChunk = data.map(storyToHtml).join('');
+    document.querySelector('#stories').innerHTML = htmlChunk;
+}
+
+const storyToHtml = story => {
+    return `<section>
+        <img src="${story.user.thumb_url}" />
+        <p>${story.user.username}</p>
+    </section>
+    `
+}
+
+const showPosts = async (token) => {
+    const endpoint = `${rootURL}/api/posts`;
+    const response = await fetch(endpoint, {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token
+        }
+    })
+    const data = await response.json();
+    console.log('Posts:', data);
+
+    const arrayOfHTML = data.map(postToHTML);
+    console.log(arrayOfHTML);
+    const htmlString = arrayOfHTML.join('');
+    document.querySelector('#posts').innerHTML = htmlString;
+}
+
+
+const showModal = () => {
+    alert('show modal');
+}
+
+const showCommentAndButtonIfItMakesSense = post => {
+    const hasComments = post.comments.length > 0;
+    const lastCommentIndex = post.comments.length - 1;
+    if (hasComments) {
+        return `<div>
+            <button onclick="showModal()">View all ${post.comments.length} comments</button>
+            <p>${post.comments[lastCommentIndex].text}</p>
+        </div>`;
+    } else {
+        return '';
+    } 
+}
+
+const postToHTML = post => {
+    // console.log(post.comments.length);
+    return `
+        <section>
+            <img src="${post.image_url}" alt="Fake image" />
+            <p>${post.caption}</p>
+            ${ showCommentAndButtonIfItMakesSense(post) }
+        </section>
+    `
+}
+
+
+const initPage = async () => {
+    // first log in (we will build on this after Spring Break):
+    const token = await getAccessToken(rootURL, 'webdev', 'password');
+    console.log(token);
+    // then use the access token provided to access data on the user's behalf
+    showStories(token);
+    showPosts(token);
+
+    // query for the user's profile
+    // query for suggestions
+}
+
+
+// Kicks off the website:
+initPage();
